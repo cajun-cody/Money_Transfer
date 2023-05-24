60adc8a1fc6d83baaa122bd2fa33bccd58d096c0
Cody <cemt5462@gmail.com>
2023-05-24 14:55:14 -0500

    Completed Money Transfer project.

---
+++ MoneyTransfer/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Person
+    {
+        public string name;
+        private Wallet wallet;
+
+        public Person(string name, int cash)
+        {
+            //TODO 2: Assign the value of the 'name' parameter to the 'name' member variable. One line.
+            this.name = name;
+            //TODO 3: Instantiate a new 'Wallet' object, passing the 'cash' parameter into its constructor. Assign this new Wallet to the 'wallet' member variable. One line.
+            Wallet wallet = new Wallet(cash); this.wallet = wallet;
+        }
+
+        public void DisplayInfo()
+        {
+            //TODO 4: Use string interpolation in a ConsoleWriteLine to display the user's name and also the amount of money in their wallet (using dot notation). One line.
+            Console.WriteLine($"{name} has ${wallet.money}.");
+        }
+
+        public void AcceptMoney(int amount)
+        {
+            //TODO 7: Use an assignment operator to increase the amount of money in the person's wallet by the value of the 'amount' parameter. One line.
+            this.wallet.money += amount;
+        }
+
+        public bool TransferMoney(Person receiver, int amountToTransfer)
+        {
+            //TODO 8: Use a conditional to check if the 'amountToTransfer' parameter of this method is less than or equal to the amount of money in the wallet of the giver.
+            //If the parameter is less than the amount in the wallet, subtract that amount from the wallet, then call the receiver's AcceptMoney() method and pass 'amountToTransfer' into it. Also use a Console.WriteLine to describe the transaction that occurred.
+            //If the parameter is greater than the amount in the giver's wallet, do not transfer any money and instead print a message describing why the transfer failed.
+            Console.WriteLine($"{name} wants to send {receiver.name} money!");
+
+            if (amountToTransfer <= this.wallet.money) //Check if the transfer is less than the wallet
+            {
+                this.wallet.money -= amountToTransfer; //Subtract from my wallet.
+                receiver.AcceptMoney(amountToTransfer); //Pass in the amount to transfer as a param.
+                Console.WriteLine("Transaction Successful!");
+                Console.WriteLine($"{name} has sent {receiver.name} {amountToTransfer} dollars.");
+                return true;
+            }
+            else if (amountToTransfer > this.wallet.money) //Check if the transfer is more than the wallet
+            {
+                Console.WriteLine("Insufficient Funds!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Error 404 Not Found");
+                return false;
+            }
+
+        }
+        public string ChooseFriend()
+        {
+            Console.WriteLine("Choose a friend you want to help! cody, jenny, jordy, jack or jon");
+            string choice = Console.ReadLine();
+            return choice;
+        }
+
+
+    }
+}
---
+++ MoneyTransfer/Simulation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Simulation
+    {
+        public Simulation()
+        {
+            
+        }
+
+        public void RunSimulation()
+        {
+            Console.WriteLine("Starting simulation");
+            //TODO 5: Instantiate two Person objects, with unique names and money amounts. Two lines.
+            Person cody = new Person("cody", 1000);
+            Person jenny = new Person("Jenny", 850);
+            Person jordy = new Person("jordy", 500);
+            Person jack = new Person("jack", 450);
+            Person jon = new Person("jon", 800);
+
+            //TODO 6: Call the .DisplayInfo() method on each person object, one at a time. This will show their initial money amount. Two lines.
+            cody.DisplayInfo();
+            jenny.DisplayInfo();
+            jordy.DisplayInfo();
+            jack.DisplayInfo();
+            jon.DisplayInfo();
+
+            //TEST THE APPLICATION AFTER TODOS 5 AND 6!!
+
+            //TODO 9: Call the .TransferMoney() method on one person object (giver), making sure to pass in the second person object (receiver), and the amount being transfered. One line.
+            //Bonus
+            string choice =cody.ChooseFriend();  //Choice is holding a string value of ChooseFriend.
+            
+            if (choice == jordy.name)
+            {
+                cody.TransferMoney(jordy, 500);
+            }
+            else if (choice == jack.name)
+            {
+                cody.TransferMoney(jack, 1001);
+            }
+            else if (choice == jenny.name)
+            {
+                cody.TransferMoney(jenny, 1001);
+            }
+            else if (choice == jon.name)
+            {
+                cody.TransferMoney(jon, 500);
+            }
+            else
+            {
+                Console.WriteLine("Choose a name from the list.");
+                cody.ChooseFriend();
+            }
+
+
+            //TODO 10: Call the .DisplayInfo() method on each person object again, showing that each person's balance has changed. Two lines.
+            cody.DisplayInfo();
+            jenny.DisplayInfo();
+            jordy.DisplayInfo();
+            jack.DisplayInfo();
+            jon.DisplayInfo();
+
+            Console.WriteLine("Simulation complete.");
+        }
+
+
+    }
+}
